7155913b8cffc00ac8379f0632d4321471280083
FranQueue <fquagliotti86@gmail.com>
2025-05-06 21:27:42 +0200

    Fix: Added multer-storage-cloudinary dependency

--- cloudinary.js
+++ cloudinary.js
@@ -4,8 +4,7 @@ const multer = require('multer');
 
 // Configure Cloudinary with your credentials
 cloudinary.config({
-  cloud_name: 	
-  franqueue,
+  cloud_name: franqueue,
   api_key: 755166464866419,
   api_secret: ZzDEuJ9-s2gobRpjh6WayOmLHh8,
 });
